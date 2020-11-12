778c835876d6a19dcddc15e904c35c10a899b71f
almilasultantas <almilasultantas@outlook.com>
2020-11-13 01:48:09 +0300

    adding files

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div >
+    <Navbar/>
+    <Sidebar/>
+    <Login/>
+    <Cart/>
+  </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue"
+import Sidebar from "./components/Sidebar.vue"
+import Login from  "./components/Login.vue"
+import Cart from "./components/Cart.vue"
+
+export default {
+  components: {
+    Navbar,
+    Sidebar,
+    Login,
+    Cart
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
